0f94fa49d355d864f90c75d9be3935ef5e02b8fb
Umar <ozodovumarjon95@gmail.com>
2024-04-07 16:18:05 +0500

    save

---
+++ assets/Discount-ClRKGHRf.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as o,o as s,F as c,a as e}from"./index-C4cGHPeH.js";const a={},n=e("h1",null,"СКИДКИ",-1),r=e("h2",{class:"page__title"},"THIS PAGE WILL BE ADDED SOON",-1);function _(l,i){return s(),o(c,null,[n,r],64)}const d=t(a,[["render",_]]);export{d as default};
---
+++ assets/Types-C5AA2ngS.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as s,o,F as c,a as e}from"./index-C4cGHPeH.js";const a={},n=e("h1",null,"КАТЕГОРИИ",-1),r=e("h2",{class:"page__title"},"THIS PAGE WILL BE ADDED SOON",-1);function _(l,f){return o(),s(c,null,[n,r],64)}const p=t(a,[["render",_]]);export{p as default};
